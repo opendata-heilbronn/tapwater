d18aa646d82a1482614458d58ff21cc580842c29
Felix Ebert <felix.ebert@if-core.de>
2015-04-30 21:03:19 +0200

    frontend - basic stuff

---
+++ frontend/modules/PlaceCtrl.js
@@ -0,0 +1,6 @@
+(function (angular) {
+    'use strict';
+    angular.module('tw').controller('PlaceCtrl', function($scope) {
+
+    });
+})(angular);
--- frontend/modules/tw.js
+++ frontend/modules/tw.js
@@ -1,10 +1,21 @@
+var tw = {
+    data: {}
+};
 (function (angular) {
     'use strict';
-    angular.module('tw', ['ngRoute']);
-    angular.module('tw').config(function ($locationProvider, $httpProvider) {
-        $locationProvider.html5Mode(true);
-    });
+    angular.module('tw', ['ngRoute', 'ngMaterial']);
     angular.module('tw').run(function ($rootScope) {
         $rootScope.page = {title: ''};
+    });
+    angular.module('tw').config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'SelectPlaceCtrl',
+                templateUrl: 'modules/partials/selectPlace.html'
+            })
+            .when('/place/:id', {
+                controller: 'PlaceCtrl',
+                templateUrl: 'modules/partials/place.html'
+            });
     });
 })(angular);
