e77b44b1c0d552b1cd6cc74a3ca0a021c2ed8e64
Adrian Stabiszewski <as@grundid.de>
2015-04-30 20:25:39 +0200

    first entity

--- backend/src/main/java/de/opendatahn/tabwater/entity/CityRepository.java
+++
@@ -1,7 +0,0 @@
-package de.opendatahn.tabwater.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CityRepository extends JpaRepository<City, Long> {
-
-}
--- backend/src/main/java/de/opendatahn/tabwater/entity/Substance.java
+++
@@ -1,5 +0,0 @@
-package de.opendatahn.tabwater.entity;
-
-public class Substance {
-
-}
