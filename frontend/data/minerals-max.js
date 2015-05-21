5c4a220fd31b4d9dabb65f263aafb78e7170ac3f
gravima <jung@gravima.de>
2015-05-21 21:55:20 +0200

    Added mineral gauge Please enter the commit message for your changes. Lines starting

---
+++ frontend/data/minerals-max.js
@@ -0,0 +1,10 @@
+tw.data.mineralMax = {
+	"natrium": 400,
+	"kalium": 300,
+	"calcium": 300,
+	"magnesium": 100,
+	"chlorid": 100,
+	"nitrat": 100,
+	"sulfat": 100,
+	"hardness": 100
+};
--- frontend/modules/PlaceCtrl.js
+++ frontend/modules/PlaceCtrl.js
@@ -3,5 +3,11 @@
     angular.module('tw').controller('PlaceCtrl', function($scope, $routeParams) {
         $scope.placeID = $routeParams.id;
         $scope.placevalues = tw.data.zones[$routeParams.id];
+        $scope.calculateHeight = function(key, value) {
+            console.log (tw.data.mineralMax)
+            var mineralMax = tw.data.mineralMax[key];
+            var calculatedHeight = 400 / mineralMax * value;
+            return calculatedHeight;
+        }
     });
 })(angular);
